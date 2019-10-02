214b13053f3a15c62c1b020b6255e1f51231f240
Nicolás Marino <nicolasmarino.17@gmail.com>
2019-10-02 18:43:22 -0300

    Update Listas.hs

--- Listas.hs
+++ Listas.hs
@@ -120,3 +120,77 @@ elementoDePosDada2 (x:xs) a
                | a == x = 0
                | otherwise = (1+(elementoDePosDada2 xs a)) 
     
+-- 15
+
+cumpleCond::[a]->(a->Bool)->[a]
+cumpleCond [] f = []
+cumpleCond (x:xs) f
+            | f x = cumpleCond xs f
+            | otherwise = cumpleCond xs f
+
+-- dada una lista y un predicado, retornar los primeros n elementos contiguos de la lista, tal que todos los elementos cumplem con el predicado.
+-- ejemplo: tomarMientras (esPar) [2,4,6,8,9,2] = [2,4,6]
+esPar::Integral a => a->Bool
+esPar a = (mod a 2) == 0
+
+predicadoContiguo::Eq a =>[a]->(a->Bool)->[a]
+predicadoContiguo [] p = []
+predicadoContiguo (x:xs) p
+                | ((p x) == True) = (x : (predicadoContiguo xs p))
+                | otherwise = []
+
+-- Programar una funcion que dada una lista y un elemento elimine todas las ocurrencias del elemento en la lista.
+
+borrarElemLista::Eq a =>[a]->a->[a]
+borrarElemLista [] x = []
+borrarElemLista (x:xs) b 
+                | x == b = (borrarElemLista xs b)
+                | otherwise = x : (borrarElemLista xs b)
+
+-- programar funcion que dada una lista y un elemento elimine la primer ocurrencia del elemento.
+
+borrarPrimerElem::Eq a =>[a]->a->[a]
+borrarPrimerElem [] x = []
+borrarPrimerElem (x:xs) b 
+                | x == b = xs
+                | otherwise = x : (borrarPrimerElem xs b)
+
+            
+
+-- Programar tonar n qe dada una lista y un numero retorna los primeros n elementos de la lista
+
+nElem::Eq a =>[a]->Int->[a]
+nElem [] b = []
+nElem (x:xs) b
+        | (b /= 0) = x : (nElem xs (b-1))
+        | otherwise = nElem xs b
+
+-- 18.a Programar tomar n que dada una lista y un numero retorna la lista  resultado de descartar los primeros n elementos
+borrarNelem::Eq a => [a]->Int->[a]
+borrarNelem [] b = []
+borrarNelem (x:xs) b
+            | (b == 0) = x:xs
+            | otherwise = (borrarNelem xs (b-1))
+
+
+-- 18.b Programar fromTo, que dados a <= b, retorne la lista de todos los numeros entre ay b inclusive. EJ: fromTo 1 3 = [1,2,3]
+fromTo::Int->Int->[Int]
+fromTo x y 
+        | x <= y = x : (fromTo (x+1) y)
+        | otherwise = []
+-- dado un predicado y una lista me devuelve todos los elementos que cumplan con la condicion
+filtrar::[Int]->(Int->Bool)->[Int]
+filtrar [] f = []
+filtrar (x:xs) f    
+        | (f x) = x:filtrar xs f
+        | otherwise = filtrar xs f
+-- Programar divisores que dado un numero retorna la lista de divisores de dicho numero usar filter y fromto para esto.
+divisores::Int->[Int]
+divisores n = filtrar (fromTo 1 n) (\x -> mod n x == 0)
+
+--primera posicion retorna la pos que aparece x o length si no
+primera_posicion::[Int]->Int->Int
+primera_posicion [] n = 0
+primera_posicion (x:xs) n
+        |(x == n) = 0
+        |(x /= n) = 1 + primera_posicion xs n
